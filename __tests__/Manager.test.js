b4e9cc45e8f95b50eb49084d1085ac7c2a979429
Pineapple Season <emsthai21@gmail.com>
2020-11-29 15:32:40 -0700

    successful set officeNo test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,23 @@
 const { test } = require("@jest/globals");
 const Manager = require("../lib/Manager");
 
+test("Set officeNo by constructor", () => {
+    const testOfficeNo = 21;
+    const testManager = new Manager('Emily', 21, 'notreal@email.com', testOfficeNo);
+
+    expect(testManager.officeNo).toBe(testOfficeNo);
+});
+// Refine the following code for Manager test (C&P from Intern.test.js) 
+// test("getSchool() test", () => {
+//     const testSchool = 'University of Arizona';
+//     const testManager = new Manager('Emily', 21, 'notreal@email.com', testSchool);
+
+//     expect(testManager.getSchool()).toBe(testSchool);
+// });
+
+// test("getRole() returns Manager", () => {
+//     const testRole = 'Manager';
+//     const testObj = new Manager('Emily', 21, 'notreal@email.com', 'geneskit');
+
+//     expect(testObj.getRole()).toBe(testRole);
+// });
