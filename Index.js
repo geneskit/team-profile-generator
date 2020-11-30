e8c0cf975557785c672d6645e5f49d2f4e99dfe4
Pineapple Season <emsthai21@gmail.com>
2020-11-29 20:42:54 -0700

    set up Index outline

--- Index.js
+++ Index.js
@@ -1,11 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHtml = require('./src/htmlPage.html');
 const path = require('path');
+
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+const employeeData = [];
 
+// start generator
+function init () {
+    promptManager();
+};
+// prompt manager for questions
+function promptManager () {
+    
+};
+// ask to add another employee (engineer or intern) or to finish building team
+function addTeamMember () {
 
+};
+// engineer questions
+function addEngineer () {
+
+}
+// intern questions
+function addIntern () {
+    
+}
