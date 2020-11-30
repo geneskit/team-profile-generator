ccfa9474797a846aa9ab75e81ad45f22177f5c3a
Pineapple Season <emsthai21@gmail.com>
2020-11-29 21:02:22 -0700

    promptManager function started

--- Index.js
+++ Index.js
@@ -15,7 +15,76 @@ function init () {
 };
 // prompt manager for questions
 function promptManager () {
-    
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'nameInput',
+            message: 'What is your name?',
+            validate: managerNameInput => 
+            {
+                if (managerNameInput) 
+                {
+                    return true;
+                }
+                else 
+                {
+                    console.log('Please enter your name! ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'idNumber',
+            message: 'What is your ID number?',
+            validate: managerIdInput => 
+            {
+                if (managerIdInput) 
+                {
+                    return true;
+                }
+                else 
+                {
+                    console.log('Please enter your ID number! ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: managerEmailInput => 
+            {
+                if (managerEmailInput) 
+                {
+                    return true;
+                }
+                else 
+                {
+                    console.log('Please enter your email address! ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'email',
+            message: 'What is your office number?',
+            validate: managerOfficeNo => 
+            {
+                if (managerOfficeNo) 
+                {
+                    return true;
+                }
+                else 
+                {
+                    console.log('Please enter your office number! ');
+                    return false;
+                }
+            }
+        }
+    ])
 };
 // ask to add another employee (engineer or intern) or to finish building team
 function addTeamMember () {
